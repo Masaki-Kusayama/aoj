34a986585db853377bb5bc7510c5de2f30ce5baa
Masaki Kusayama <kusayama@retty.me>
2015-12-03 02:05:49 +0900

    add Small, Large, or Equal

---
+++ itp1/php/2_a-01.php
@@ -0,0 +1,12 @@
+<?php
+	$stdin = explode(" ", trim(fgets(STDIN)));
+	$a = $stdin[0];
+	$b = $stdin[1];
+
+	if ($a > $b) {
+		echo "$a > $b" . PHP_EOL;
+	} else if ($a < $b) {
+		echo "$a < $b" . PHP_EOL;
+	} else {
+		echo "$a == $b" . PHP_EOL;
+	}
